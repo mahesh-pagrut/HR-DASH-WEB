1109ec6107eea063e053eef472c5a008028dbedd
mahesh-pagrut <maheshdpmayshu@gmail.com>
2025-08-05 14:52:16 +0530

    added search query using comand k package, added routes conditional ui in sidbar

--- frontend/src/Sidebar/RouteSelect.jsx
+++ frontend/src/Sidebar/RouteSelect.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
+import {
+  FiDollarSign,
+  FiHome,
+  FiLink,
+  FiPaperclip,
+  FiUsers,
+} from 'react-icons/fi';
 
 const RouteSelect = () => {
   return (
-    <div>RouteSelect</div>
-  )
-}
+    <div className="space-y-1">
+      <Route Icon={FiHome} selected={true} title="Dashboard" />
+      <Route Icon={FiUsers} selected={false} title="Team" />
+      <Route Icon={FiPaperclip} selected={false} title="Invoices" />
+      <Route Icon={FiLink} selected={false} title="Integrations" />
+      <Route Icon={FiDollarSign} selected={false} title="Finance" />
+    </div>
+  );
+};
 
-export default RouteSelect
+const Route = ({ selected, Icon, title }) => {
+  return (
+    <button
+      className={`flex items-center justify-start gap-2 w-full rounded px-2 py-1.5 text-sm transition-[box-shadow,_background-color,_color] ${selected
+          ? 'bg-white text-stone-950 shadow'
+          : 'hover:bg-stone-200 bg-transparent text-stone-500 shadow-none'
+        }`}
+    >
+      <Icon className={selected ? 'text-violet-500' : ''} />
+      <span>{title}</span>
+    </button>
+  );
+};
+
+export default RouteSelect;
--- frontend/src/Sidebar/Sidebar.jsx
+++ frontend/src/Sidebar/Sidebar.jsx
@@ -9,7 +9,7 @@ const Sidebar = () => {
       <div className="overflow-y-scroll sticky top-4 h-[calc(100vh-32px-48px)]">
         <AccountToggle />
         <Search />
-        {/* <RouteSelect /> */}
+        <RouteSelect />
       </div>
       {/* <Plan /> */}
     </div>
